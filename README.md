e3a89ca92f4b0417197c9704aab997c88e8a368b
Steven P <s.predtet.323@gmail.com>
2025-07-30 14:15:43 -0700

    Entire Weather App project

---
+++ README.md
@@ -0,0 +1,9 @@
+Weather App
+
+Setup Instructions: 
+    Clone repository
+    Install dependencies
+    Create .env file
+    Generate app key: php artisan key:generate
+    Run server: PHP artisan serve
+
--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -8,18 +8,9 @@ use Illuminate\Http\Request;
 class WeatherController extends Controller
 {
     public function index()
-    {
-        if (!Storage::exists('weather.json')) {
-            return "File not found.";
-        }
-
-        $json = Storage::get('weather.json');
-        $weatherData = json_decode($json, true);
-
-        if ($weatherData === null) {
-            return "Failed to decode JSON.";
-        }
-
-        return view('weather.index', ['weather' => $weatherData]);
-    }
+{
+    $json = Storage::disk('public')->get('weather.json');
+    $weatherData = json_decode($json, true);
+    return view('weather.index', ['weather' => $weatherData]);
+}
 }
